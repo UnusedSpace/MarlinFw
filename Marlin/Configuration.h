5ddd7394b5da686c48057fa55935cbf524681492
UnusedSpace <63672748+UnusedSpace@users.noreply.github.com>
2021-12-08 14:58:41 +0100

    Änderung
    - Auto-Leveling - Vorheizen als Abhängigkeit
    - Auto-Leveling - Temperaturen gesetzt
    - Auto-Leveling - Grid-Points auf 5x5 erhöht

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1550,10 +1550,10 @@
 /**
  * Auto-leveling needs preheating
  */
-//#define PREHEAT_BEFORE_LEVELING
+#define PREHEAT_BEFORE_LEVELING                                                                    // Änderung
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
-  #define LEVELING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
-  #define LEVELING_BED_TEMP     50
+  #define LEVELING_NOZZLE_TEMP 205   // (°C) Only applies to E0 at this time                       // Änderung
+  #define LEVELING_BED_TEMP     60                                                                 // Änderung
 #endif
 
 /**
@@ -1602,7 +1602,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5                                                                      // Änderung
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
