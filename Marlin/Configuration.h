5e094588db327cfb02945e7eb3a36c23c3f84167
UnusedSpace <63672748+UnusedSpace@users.noreply.github.com>
2022-07-25 13:27:31 +0200

    Update Configuration.h
    Syntaxfehler

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "((UnusedSpace | MarlinFw 2.1.1.0)" // Who made the changes.        // Änderung
+#define STRING_CONFIG_H_AUTHOR "(UnusedSpace | MarlinFw 2.1.1.0)" // Who made the changes.        // Änderung
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
